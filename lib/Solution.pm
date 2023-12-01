9455319e7c07e033e3b95b9f52b2eaa2e8d0f397
bbrtj <bbrtj.pro@gmail.com>
2023-12-01 07:11:32 +0100

    More robust input handling

--- lib/Solution.pm
+++ lib/Solution.pm
@@ -1,13 +1,17 @@
 package Solution;
 
 use Types::Common -types;
-use autodie;
 
 use class;
 
 has field '_running_part' => (
 	isa => Tuple[PositiveInt, PositiveInt],
 	writer => 1,
+);
+
+has field 'input' => (
+	isa => ArrayRef[Str],
+	lazy => 'grab_input',
 );
 
 sub _init_part ($self, $part)
@@ -35,11 +39,19 @@ sub grab_input ($self, $wanted_part = undef)
 {
 	my ($day, $part) = $self->_running_part->@*;
 	$part = $wanted_part if $wanted_part;
-	my $filename = "input/day${day}_part${part}.txt";
+	my @filenames = (
+		"input/day${day}_part${part}.txt",
+		"input/day${day}.txt",
+	);
 
-	open my $fh, '<', $filename;
-	my @lines = readline $fh;
+	foreach my $filename (@filenames) {
+		if (open my $fh, '<', $filename) {
+			my @lines = readline $fh;
 
-	return [grep { length } map { chomp; $_ } @lines];
+			return [grep { length } map { chomp; $_ } @lines];
+		}
+	}
+
+	die "couldn't get input for day $day part $part";
 }
 
