40fd46b340c4574d318594445839d8a52ef7cd6d
bbrtj <bbrtj.pro@gmail.com>
2023-12-07 08:15:53 +0100

    Optimize day 7

--- lib/Day7/Solution.pm
+++ lib/Day7/Solution.pm
@@ -4,7 +4,6 @@ use List::Util qw(sum);
 use builtin qw(indexed);
 
 use class;
-no warnings qw(experimental::builtin);
 
 with 'Solution';
 
@@ -12,7 +11,8 @@ sub _parse_input ($self, $input = $self->input)
 {
 	my @data;
 	foreach my $line ($self->input->@*) {
-		push @data, [split / /, $line];
+		my ($cards, $value) = split / /, $line;
+		push @data, [[split //, $cards], $value];
 	}
 
 	return \@data;
@@ -22,20 +22,21 @@ sub get_set_value ($self, $set, $use_jokers)
 {
 	state @cards_normal = qw(A K Q J T 9 8 7 6 5 4 3 2);
 	state @cards_jokers = qw(A K Q T 9 8 7 6 5 4 3 2 J);
+	state %cards_map_normal = map { $cards_normal[$_] => @cards_normal - $_ } keys @cards_normal;
+	state %cards_map_jokers = map { $cards_jokers[$_] => @cards_jokers - $_ } keys @cards_jokers;
 	state $cards_in_set = 5;
 
 	# work out the basics
-	my @cards = $use_jokers ? @cards_jokers : @cards_normal;
-	my %cards_map = map { $cards[$_] => @cards - $_ } keys @cards;
+	my $cards_map = $use_jokers ? \%cards_map_jokers : \%cards_map_normal;
+	my $cards_count = keys $cards_map->%*;
 	my %card_types;
-	my @these_cards = split //, $set;
 
 	# calculate cards value and count cards for later
 	my $cards_value = 0;
-	foreach my ($index, $card) (indexed @these_cards) {
+	foreach my ($index, $card) (indexed $set->@*) {
 		$card_types{$card}++;
 		my $power = ($cards_in_set - $index - 1);
-		$cards_value += $cards_map{$card} * ((@cards + 1) ** $power);
+		$cards_value += $cards_map->{$card} * (($cards_count + 1) ** $power);
 	}
 
 	# these subs will match a set, their index is a set value
