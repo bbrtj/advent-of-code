41f43cc4db84a481e01827263d94a8e841618796
bbrtj <bbrtj.pro@gmail.com>
2023-12-07 08:02:46 +0100

    Refactor day 7

--- lib/Day7/Solution.pm
+++ lib/Day7/Solution.pm
@@ -18,38 +18,49 @@ sub _parse_input ($self, $input = $self->input)
 	return \@data;
 }
 
-sub get_set_value ($self, $set)
+sub get_set_value ($self, $set, $use_jokers)
 {
-	state @cards = qw(A K Q J T 9 8 7 6 5 4 3 2);
-	state %cards_map = map { $cards[$_] => @cards - $_ } keys @cards;
+	state @cards_normal = qw(A K Q J T 9 8 7 6 5 4 3 2);
+	state @cards_jokers = qw(A K Q T 9 8 7 6 5 4 3 2 J);
+	state $cards_in_set = 5;
+
+	# work out the basics
+	my @cards = $use_jokers ? @cards_jokers : @cards_normal;
+	my %cards_map = map { $cards[$_] => @cards - $_ } keys @cards;
+	my %card_types;
 	my @these_cards = split //, $set;
 
-	state $cards_in_set = 5;
-	state $card_value_increase = @cards + 1;
-
+	# calculate cards value and count cards for later
 	my $cards_value = 0;
-	my %card_types;
 	foreach my ($index, $card) (indexed @these_cards) {
 		$card_types{$card}++;
 		my $power = ($cards_in_set - $index - 1);
-		$cards_value += $cards_map{$card} * ($card_value_increase ** $power);
+		$cards_value += $cards_map{$card} * ((@cards + 1) ** $power);
 	}
 
-	my @type_counts = sort { $b <=> $a } values %card_types;
-	my @set_type_values = (
-		sub { $type_counts[0] == 5 },
-		sub { @type_counts == 2 && $type_counts[0] == 4 },
-		sub { @type_counts == 2 && $type_counts[0] == 3 },
-		sub { @type_counts == 3 && $type_counts[0] == 3 },
-		sub { @type_counts == 3 && $type_counts[0] == 2 },
-		sub { @type_counts == 4 },
-		sub { 1 },
+	# these subs will match a set, their index is a set value
+	# first value is number of different card types
+	# second value is highest number of same cards
+	state @set_type_values = (
+		sub {               $_[1] == 5 },
+		sub {               $_[1] == 4 },
+		sub { $_[0] == 2 && $_[1] == 3 },
+		sub { $_[0] == 3 && $_[1] == 3 },
+		sub { $_[0] == 3 && $_[1] == 2 },
+		sub { $_[0] == 4               },
 	);
 
-	my $set_value;
+	# calculate values for the anonymous subs
+	my $jokers = $use_jokers ? (delete $card_types{J}) // 0 : 0;
+	my @type_counts = sort { $b <=> $a } values %card_types;
+	my $total_card_types = @type_counts;
+	my $highest_card_count = ($type_counts[0] // 0) + $jokers;
+
+	# find out this set value
+	my $set_value = 0;
 	foreach my ($index, $test) (indexed @set_type_values) {
-		if ($test->()) {
-			$set_value = $#set_type_values - $index;
+		if ($test->($total_card_types, $highest_card_count)) {
+			$set_value = @set_type_values - $index;
 			last;
 		}
 	}
@@ -57,98 +68,41 @@ sub get_set_value ($self, $set)
 	return [$set_value, $cards_value];
 }
 
-sub get_set_value_with_jokers ($self, $set)
+sub sort_sets ($self, $input, $use_jokers)
 {
-	state @cards = qw(A K Q T 9 8 7 6 5 4 3 2 J);
-	state %cards_map = map { $cards[$_] => @cards - $_ } keys @cards;
-	my @these_cards = split //, $set;
+	my @valued_sets;
 
-	state $cards_in_set = 5;
-	state $card_value_increase = @cards + 1;
-
-	my $cards_value = 0;
-	my %card_types;
-	foreach my ($index, $card) (indexed @these_cards) {
-		$card_types{$card}++;
-		my $power = ($cards_in_set - $index - 1);
-		$cards_value += $cards_map{$card} * ($card_value_increase ** $power);
+	foreach my $set ($input->@*) {
+		my ($cards, $value) = $set->@*;
+		push @valued_sets, [$self->get_set_value($cards, $use_jokers), $value, $cards];
 	}
 
-	my $jokers = (delete $card_types{J}) // 0;
-	my @type_counts = sort { $b <=> $a } values %card_types;
-	$type_counts[0] += $jokers;
-	my @set_type_values = (
-		sub { $type_counts[0] == 5 },
-		sub { @type_counts == 2 && $type_counts[0] == 4 },
-		sub { @type_counts == 2 && $type_counts[0] == 3 },
-		sub { @type_counts == 3 && $type_counts[0] == 3 },
-		sub { @type_counts == 3 && $type_counts[0] == 2 },
-		sub { @type_counts == 4 },
-		sub { 1 },
-	);
-
-	my $set_value;
-	foreach my ($index, $test) (indexed @set_type_values) {
-		if ($test->()) {
-			$set_value = $#set_type_values - $index;
-			last;
-		}
-	}
-
-	return [$set_value, $cards_value];
+	return [
+		sort {
+			# by set
+			$a->[0][0] <=> $b->[0][0]
+			||
+			# by cards
+			$a->[0][1] <=> $b->[0][1]
+		} @valued_sets
+	];
 }
 
 sub part_1 ($self)
 {
 	my $input = $self->_parse_input;
-	my @valued_sets;
-
-	foreach my $set ($input->@*) {
-		my ($cards, $value) = $set->@*;
-		push @valued_sets, [$self->get_set_value($cards), $value, $cards];
-	}
-
-	my @set_sorted = sort {
-		# by set
-		my $sorted = $a->[0][0] <=> $b->[0][0];
-
-		if ($sorted == 0) {
-			# by $cards
-			$a->[0][1] <=> $b->[0][1];
-		}
-		else {
-			$sorted;
-		}
-	} @valued_sets;
+	my $set_sorted = $self->sort_sets($input, !!0);
 
 	my $multiplier = 1;
-	return sum map { $multiplier++ * $_->[1] } @set_sorted;
+	return sum map { $multiplier++ * $_->[1] } $set_sorted->@*;
 }
 
 sub part_2 ($self)
 {
 	my $input = $self->_parse_input;
-	my @valued_sets;
-
-	foreach my $set ($input->@*) {
-		my ($cards, $value) = $set->@*;
-		push @valued_sets, [$self->get_set_value_with_jokers($cards), $value, $cards];
-	}
-
-	my @set_sorted = sort {
-		# by set
-		my $sorted = $a->[0][0] <=> $b->[0][0];
-
-		if ($sorted == 0) {
-			# by $cards
-			$a->[0][1] <=> $b->[0][1];
-		}
-		else {
-			$sorted;
-		}
-	} @valued_sets;
+	my $set_sorted = $self->sort_sets($input, !!1);
 
 	my $multiplier = 1;
-	return sum map { $multiplier++ * $_->[1] } @set_sorted;
+	return sum map { $multiplier++ * $_->[1] } $set_sorted->@*;
 }
 
