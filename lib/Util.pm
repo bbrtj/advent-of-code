ad247dbe22edb5cfe4092a323c72b6ac5640fa2f
bbrtj <bbrtj.pro@gmail.com>
2023-12-12 20:42:07 +0100

    Day 12: speed up by parallelizing

--- lib/Util.pm
+++ lib/Util.pm
@@ -1,8 +1,13 @@
 package Util;
 
 use Term::ANSIColor;
+use Exporter qw(import);
 
 use header;
+
+our @EXPORT_OK = qw(
+	parallel_map
+);
 
 sub print_time ($, $time, %options)
 {
@@ -24,5 +29,52 @@ sub print_time ($, $time, %options)
 	my $time_μs = int(($time * 1_000_000)) % 1000;
 	$time = sprintf '%ss %3sms %3sμs', $time_s, $time_ms, $time_μs;
 	say "$options{header}: " . colored($time, $time_color);
+}
+
+sub parallel_map :prototype(&@) ($sub, @items)
+{
+	my @pids;
+	my @handles;
+	my $forks = 8;
+
+	# set up forks
+	for my $fork_no (0 .. $forks - 1) {
+		my $pid = open(my $handle, "-|")
+			// die "Can't fork: $!";
+
+		if ($pid) {
+			# parent
+			push @pids, $pid;
+			push @handles, $handle;
+		}
+		else {
+			# child
+			my @results;
+			for (my $i = $fork_no; $i < @items; $i += $forks) {
+				local $_ = $items[$i];
+				push @results, $sub->();
+			}
+
+			say join "\x00", @results;
+			exit;
+		}
+	}
+
+	# gather results
+	for my $fork_no (0 .. $forks - 1) {
+		my $output = readline $handles[$fork_no];
+		chomp $output;
+		close $handles[$fork_no] or die "Can't close: $!";
+
+		my $last = $fork_no;
+		foreach my $result (split /\x00/, $output) {
+			$items[$last] = $result;
+			$last += $forks;
+		}
+
+		waitpid $pids[$fork_no], 0;
+	}
+
+	return @items;
 }
 
